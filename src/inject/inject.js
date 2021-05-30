d23fffbe70f5033b5e8eeeb1d1524f6d43ed7291
Tina van Schelt <tinavanschelt@gmail.com>
2021-05-30 16:16:56 +0200

    feat: add face detection to inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,10 +3,12 @@ chrome.extension.sendMessage({}, function (response) {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
 
+      const bodyEl = document.querySelector("body");
+
       const videoEl = document.createElement("video");
       videoEl.setAttribute("id", "videoEl");
       videoEl.setAttribute("autoplay", "true");
-      document.querySelector("body").appendChild(videoEl);
+      bodyEl.appendChild(videoEl);
 
       if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices
@@ -18,6 +20,33 @@ chrome.extension.sendMessage({}, function (response) {
             console.log("Something went wrong!");
           });
       }
+
+      videoEl.onloadeddata = async (e) => {
+        console.log("Yay!", e);
+
+        if (faceLandmarksDetection) {
+          // Load the faceLandmarksDetection model assets.
+          const model = await faceLandmarksDetection.load(
+            faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
+          );
+
+          // Pass in a video stream to the model to obtain an array of detected faces from the MediaPipe graph.
+          // For Node users, the `estimateFaces` API also accepts a `tf.Tensor3D`, or an ImageData object.
+          const faces = await model.estimateFaces({ input: e.target });
+          console.log(faces);
+
+          for (let i = 0; i < faces.length; i++) {
+            const keypoints = faces[i].scaledMesh;
+
+            // Log facial keypoints.
+            for (let i = 0; i < keypoints.length; i++) {
+              const [x, y, z] = keypoints[i];
+
+              console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+            }
+          }
+        }
+      };
     }
   }, 10);
 });
