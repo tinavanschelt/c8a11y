142c7e659a46dc7e0ac762b27424aec3f130673c
Tina van Schelt <tinavanschelt@gmail.com>
2021-05-30 12:00:51 +0200

    feat: add video stream

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,28 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+      const videoEl = document.createElement("video");
+      videoEl.setAttribute("id", "videoEl");
+      videoEl.setAttribute("autoplay", "true");
+      document.querySelector("body").appendChild(videoEl);
+	  
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia({ video: true })
+          .then(function (stream) {
+            videoEl.srcObject = stream;
+          })
+          .catch(function (err0r) {
+            console.log("Something went wrong!");
+          });
+      }
 
-	}
-	}, 10);
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      console.log("Hello. This message was sent from scripts/inject.js");
+      // ----------------------------------------------------------
+    }
+  }, 10);
 });
